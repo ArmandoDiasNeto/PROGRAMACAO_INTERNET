e46950e19d2fa71f46d92eed71d8375cffbbde12
Luiz Fernando <lufmalta@gmail.com>
2018-10-24 18:55:42 -0300

    inserindo tabela produtos e model produtos e fazendo a lista e o add no banco

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Produto;
+use Illuminate\Support\Facades\Auth;
 
 class ProdutosController extends Controller
 {
@@ -23,6 +25,44 @@ class ProdutosController extends Controller
      */
     public function index()
     {
-        return view('produtos');
+    	
+    	$dono = Auth::user()->email;
+    	$lista = Produto::where('dono', $dono)->orderBy('item', 'desc')->get();
+    	$array = array('lista'=> $lista);
+    	//quando entrar aqui em produtos, ele vai gerar
+    	//o formulario para insercao de produtos
+        
+        // para pegar os dados do produto é so usar
+        // $produto = Produto::all();
+        // com esse comando ele vai no banco e pega todos os produto
+        // se quiser selecionar um produto x
+        // usar $produto = Produto::where('id', 'o id que voce quer');
+       	return view('produtos', $array);// e depois de produtos vou colocar o array 
+       	// com os dados do banco;
+    }
+    public function add(){
+    	if(!empty($_POST['item']) && !empty($_POST['preco'])){
+    		$item = $_POST['item'];
+    		$valor = $_POST['preco'];
+            $dono = Auth::user()->email;           
+            $produto = new Produto;
+            $produto->item = $item;
+            $produto->valor = $valor;
+            $produto->dono = $dono;
+            $produto->save();
+            return redirect()->route('produtos');
+    	}else{
+    		// aqui posso fazer ele voltar pra pagina 
+    		// e dar uma alerta de que esta errado alguma coisa
+    		echo "Nao mandou ambos";
+    	}
+    	
+    	//primeiro pego os dados
+    	// depois vou inserir no model
+    	//e depois inserir no banco de dados.
+    	//por ultimo redireciono para a pagina home.
+    	//enviando os dados que inseri no model.
+  		
+    	//return redirect()->route('home');
     }
 }
--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,17 +1,47 @@
 @extends('layouts.app')
-
+<style type="text/css">
+		.separaDiv{
+			width:100%;
+			height:2px;
+			background-color:#CCC;
+		}
+</style>
 @section('content')
 	<div class="container">
 		<h1>Novo Produto</h1>
-		<form method="POST">
+		<form method="POST" action="{{ route('produtos')}}">
+			{{ csrf_field() }}
 			<div class="form-group">
-				<label for="produto">Produto</label>
-				<input id="produto" type="text" name="produto" class="form-control" maxlength="50">
+				<label for="item">Produto</label>
+				<input id="item" type="text" name="item" class="form-control" maxlength="50">
 			</div>
 			<div class="form-group">
 				<label for="preco">Preço</label>
 				<input id="preco" type="text" name="preco" class="form-control" maxlength="10">
 			</div>
+			<div class="form-group">
+				<input type="submit" value="Cadastrar">
+			</div>	
 		</form>
+		<div class="separaDiv"></div>
+		<h3>Lista Produtos:</h3>
+		@if(count($lista) > 0)
+		<table class="table">
+			<tr>
+				<th>Produto</th>
+				<th>Valor</th>
+				<th>Editar</th>
+				<th>Excluir</th>
+			</tr>
+			@foreach($lista as $item)
+			<tr>
+				<td>{{ $item->item }}</td>
+				<td>{{ $item->valor }}</td>
+				<td><a href="#">Editar</a></td>
+				<td><a href="#">Excluir</a></td>
+			</tr>			    
+			@endforeach
+		</table>
+		@endif
 	</div>
 @endsection
